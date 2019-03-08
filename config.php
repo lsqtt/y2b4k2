233c46dccb1dc5e19b9d059f1c643998d9540887
lsqtt <48283114+lsqtt@users.noreply.github.com>
2019-03-08 13:43:53 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('APIKEY', 'AIzaSyAlmsVKJzRAdY5Vr73bKdZNWm4sQvo_Dlw');
+define('APIKEY', 'AIzaSyCp8gZc9LObqwz0q1SvUGgWlbcwVjZAvCU');
 define('GJ_CODE', 'HK');
 define('SITE_NAME', '优管');
 define('TITLENAME', '优管');
